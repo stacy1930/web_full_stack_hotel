a3cd7173a810d6b2980df55adac3d405a47d4059
stacy perales <stacy.perales@ynov.com>
2020-11-06 09:47:20 +0100

    bootstrap-mise en place

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+
+        $manager->flush();
+    }
+}
